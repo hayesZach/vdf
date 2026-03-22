436985d7085eaba65c9b87f713e45880dbcd7ef3
Zach Hayes <hayes.Zach@protonmail.com>
2026-03-21 17:49:59 -0700

    fix tests

--- keyvalue_test.go
+++ keyvalue_test.go
@@ -17,34 +17,32 @@ func loadFile(path string) ([]byte, error) {
 	return io.ReadAll(f)
 }
 
-func parseFile(path string) (*KeyValue, error) {
+func parseFile(path string) (*Document, error) {
 	data, err := loadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	var kv KeyValue
-	if err := Unmarshal(data, &kv); err != nil {
+	var doc Document
+	if err := Unmarshal(data, &doc); err != nil {
 		return nil, err
 	}
-	return &kv, nil
+	return &doc, nil
 }
 
 func TestKeyValue_Traversal(t *testing.T) {
 	t.Parallel()
 
 	path := "./static/items_game.txt"
-	kv, err := parseFile(path)
+	doc, err := parseFile(path)
 	if err != nil {
 		t.Fatalf("failed to parse file: %v", err)
 	}
-
-	index := buildIndex(kv.Value.([]*KeyValue))
 
 	// prefabs := kv.GetAll("prefabs")
 	// if prefabs == nil {
 	// 	t.Fatalf("default rarity value is nil")
 	// }
 
-	fmt.Printf("%+v\n", index)
+	fmt.Printf("%+v\n", doc)
 }
